be752adff217a577c892b466d38880ec3db42189
codethereforam <codethereforam@gmail.com>
2019-07-28 10:42:30 +0800

    feat: 添加对象更新比较器

---
+++ java/src/main/java/priv/thinkam/toycode/copypaste/UpdateDiffer.java
@@ -0,0 +1,90 @@
+package priv.thinkam.toycode.copypaste;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+/**
+ * 对象更新比较器
+ *
+ * @param <T> 待比较的对象类型
+ * @author yanganyu
+ * @date 2019/7/26
+ */
+public final class UpdateDiffer<T> {
+    /**
+     * 原来的对象
+     */
+    private final T original;
+    /**
+     * 要更新的对象
+     */
+    private final T toUpdate;
+    /**
+     * ”原来的对象“和“要更新的对象”比较出来用于更新的对象
+     */
+    private T difference;
+    /**
+     * 需要比较的字段的get方法
+     */
+    private final List<Function<T, ?>> getMethodList;
+
+    public UpdateDiffer(T original, T toUpdate, Class<T> objectClass) {
+        Objects.requireNonNull(original);
+        Objects.requireNonNull(toUpdate);
+        Objects.requireNonNull(objectClass);
+        if (original.getClass() != toUpdate.getClass()) {
+            throw new IllegalArgumentException("original object class must be same as toUpdate object class");
+        }
+        try {
+            this.difference = objectClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            // 转成非受检异常，从而被统一异常处理
+            throw new RuntimeException("UpdateDiffer new instance of objectClass fail", e);
+        }
+        this.original = original;
+        this.toUpdate = toUpdate;
+        getMethodList = new ArrayList<>();
+    }
+
+    /**
+     * 比较字段是否相同，如果不同，把要更新的对象字段值设置到difference对象里
+     *
+     * @param getMethod get方法
+     * @param setMethod set方法
+     * @param <R>       get方法的返回值类型/set方法参数类型
+     * @return this
+     */
+    public <R> UpdateDiffer<T> diff(Function<T, R> getMethod, BiConsumer<T, R> setMethod) {
+        Objects.requireNonNull(getMethod);
+        Objects.requireNonNull(setMethod);
+        R toUpdateValue = getMethod.apply(toUpdate);
+        R originalValue = getMethod.apply(original);
+        Objects.requireNonNull(originalValue, "数据库中的字段不应该为null");
+        if (!originalValue.equals(toUpdateValue)) {
+            setMethod.accept(difference, toUpdateValue);
+        }
+        // 保存已经调用的get方法
+        getMethodList.add(getMethod);
+        return this;
+    }
+
+    /**
+     * 获取”原来的对象“和“要更新的对象”比较出来的对象，用于去数据库更新（更新前还要再设置id等字段）。
+     *
+     * @return ”原来的对象“和“要更新的对象”比较出来用于更新的对象。如果“原来的对象”和“要更新的对象”中所有要比较的字段都相同，返回null
+     */
+    public T getDifference() {
+        // 如果difference对象中所有要比较的字段都为null
+        if (
+                getMethodList.stream()
+                        .map(getFunction -> getFunction.apply(difference))
+                        .allMatch(Objects::isNull)
+        ) {
+            return null;
+        }
+        return this.difference;
+    }
+}
---
+++ java/src/test/java/priv/thinkam/toycode/copypaste/UpdateDifferTest.java
@@ -0,0 +1,141 @@
+package priv.thinkam.toycode.copypaste;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * UpdateDiffer test
+ *
+ * @author yanganyu
+ * @date 2019/7/27
+ */
+public class UpdateDifferTest {
+    private UserDAO userDAO = new UserDAO();
+
+    @Test
+    public void demonstrateStupidWay() {
+        User toUpdate = new User(1, "uu1", 20, null);
+        System.out.println("toUpdate user: " + toUpdate);
+        User original = userDAO.getById(toUpdate.getId());
+        System.out.println("original user: " + original);
+        if (original != null) {
+            User updateUser = getUserUpdate(toUpdate, original);
+            if (updateUser != null) {
+                updateUser.setId(toUpdate.getId());
+                System.out.println("update user: " + updateUser);
+                userDAO.updateById(updateUser);
+                System.out.println("updated user: " + userDAO.getById(toUpdate.getId()));
+            }
+        } else {
+            // insert user
+        }
+    }
+
+    private User getUserUpdate(User toUpdate, User original) {
+        User updateUser = new User();
+        if (!original.getName().equals(toUpdate.getName())) {
+            updateUser.setName(toUpdate.getName());
+        }
+        if (!original.getAge().equals(toUpdate.getAge())) {
+            updateUser.setAge(toUpdate.getAge());
+        }
+        if (Stream.of(updateUser.getName(), updateUser.getAge())
+                .allMatch(Objects::isNull)) {
+            // 没有更新
+            return null;
+        }
+        return updateUser;
+    }
+
+    @Test
+    public void demonstrateUpdateDiffer() {
+        User toUpdate = new User(1, "uu1", 20, null);
+        System.out.println("toUpdate user: " + toUpdate);
+        User original = userDAO.getById(toUpdate.getId());
+        System.out.println("original user: " + original);
+        if (original != null) {
+            User updateUser = new UpdateDiffer<>(original, toUpdate, User.class)
+                    .diff(User::getName, User::setName)
+                    .diff(User::getAge, User::setAge)
+                    .getDifference();
+            if (updateUser != null) {
+                updateUser.setId(toUpdate.getId());
+                System.out.println("update user: " + updateUser);
+                userDAO.updateById(updateUser);
+                System.out.println("updated user: " + userDAO.getById(toUpdate.getId()));
+            }
+        } else {
+            // insert user
+        }
+    }
+
+    @Test
+    public void testUpdateDiffer1() {
+        User toUpdate = new User(1, "uu1", 20, null);
+        User original = userDAO.getById(toUpdate.getId());
+        Assert.assertNotNull(original);
+        User updateUser = new UpdateDiffer<>(original, toUpdate, User.class)
+                .diff(User::getName, User::setName)
+                .diff(User::getAge, User::setAge)
+                .getDifference();
+        Assert.assertNotNull(updateUser);
+        updateUser.setId(toUpdate.getId());
+        userDAO.updateById(updateUser);
+        Assert.assertEquals(new User(1, "uu1", 20, 1), userDAO.getById(toUpdate.getId()));
+    }
+
+    @Test
+    public void testUpdateDiffer2() {
+        User toUpdate = new User(1, "u1", 18, null);
+        User original = userDAO.getById(toUpdate.getId());
+        Assert.assertNotNull(original);
+        User updateUser = new UpdateDiffer<>(original, toUpdate, User.class)
+                .diff(User::getName, User::setName)
+                .diff(User::getAge, User::setAge)
+                .getDifference();
+        Assert.assertNull(updateUser);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class User {
+        private Integer id;
+        private String name;
+        private Integer age;
+        private Integer sex;
+    }
+
+    private static class UserDAO {
+        private User oneUser = new User(1, "u1", 18, 1);
+
+        User getById(Integer id) {
+            if (id != 1) {
+                return null;
+            }
+            return oneUser;
+        }
+
+        void updateById(User user) {
+            Integer id = user.getId();
+            if (id == null || id != 1) {
+                return;
+            }
+            if (user.getName() != null) {
+                oneUser.setName(user.getName());
+            }
+            if (user.getAge() != null) {
+                oneUser.setAge(user.getAge());
+            }
+            if (user.getSex() != null) {
+                oneUser.setSex(user.getSex());
+            }
+        }
+    }
+}
