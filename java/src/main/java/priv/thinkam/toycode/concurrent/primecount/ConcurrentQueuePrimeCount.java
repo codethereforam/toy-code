9b13749a93eac6b20db119f592a303c9d5a5f53f
codethereforam <codethereforam@gmail.com>
2021-07-06 16:24:24 +0800

    refactor: 修改PrimeCount concurrentQueue 并行

--- java/src/main/java/priv/thinkam/toycode/concurrent/primecount/ConcurrentQueuePrimeCount.java
+++ java/src/main/java/priv/thinkam/toycode/concurrent/primecount/ConcurrentQueuePrimeCount.java
@@ -1,17 +1,23 @@
 package priv.thinkam.toycode.concurrent.primecount;
 
-import java.util.concurrent.*;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static priv.thinkam.toycode.concurrent.primecount.Common.NUMBER_UPPER_LIMIT;
 import static priv.thinkam.toycode.concurrent.primecount.Common.mapToPrimeCount;
 
 /**
- * 多线程 blockingQueue 并行
+ * 多线程 concurrentQueue 并行
  *
  * @author yanganyu
  * @date 2021/5/19
  */
-class BlockingQueuePrimeCount {
+public class ConcurrentQueuePrimeCount {
     public static final int SPLIT_COUNT = Runtime.getRuntime().availableProcessors();
 
     public static void main(String[] args) throws Exception {
@@ -19,12 +25,19 @@ class BlockingQueuePrimeCount {
 
         ExecutorService executor = Executors.newFixedThreadPool(SPLIT_COUNT);
 
-        BlockingQueue<Integer> numbersQueue = new ArrayBlockingQueue<>(NUMBER_UPPER_LIMIT - 1);
+        // 1. 使用ConcurrentLinkedQueue
+        Queue<Integer> numbersQueue = IntStream.rangeClosed(2, NUMBER_UPPER_LIMIT)
+                .boxed().collect(Collectors.toCollection(ConcurrentLinkedQueue::new));
+
+        // 2. 使用BlockingQueue
+//        Queue<Integer> numbersQueue = new ArrayBlockingQueue<>(NUMBER_UPPER_LIMIT - 1, false,
+//                IntStream.rangeClosed(2, NUMBER_UPPER_LIMIT)
+//                        .boxed()
+//                        .collect(Collectors.toList()));
+
         // 使用ArrayDeque结果是不对的，奇怪的是效率变得很低！！！
-        // Queue<Integer> numbersQueue = new ArrayDeque<>(NUMBER_UPPER_LIMIT - 1);
-        for (int i = 2; i <= NUMBER_UPPER_LIMIT; i++) {
-            numbersQueue.add(i);
-        }
+//        Queue<Integer> numbersQueue = IntStream.rangeClosed(2, NUMBER_UPPER_LIMIT)
+//                .boxed().collect(Collectors.toCollection(ArrayDeque::new));
 
         Future<Integer>[] futures = new Future[SPLIT_COUNT];
         for (int i = 0; i < SPLIT_COUNT; i++) {
