a587d849eff720748e188693089fc27c5aa175fc
codethereforam <codethereforam@gmail.com>
2021-08-06 16:31:37 +0800

    feat: add some code for tutorial.scm

--- tutorial/tutorial.scm
+++ tutorial/tutorial.scm
@@ -13,3 +13,53 @@
     (+ x 1))
 (define t (compose f g))
 (t 2)
+
+(define (factorial-iterator counter from to)
+    (if (> from to)
+        counter
+        (factorial-iterator (* counter from) (+ from 1) to)))
+
+(factorial-iterator 1 1 5)
+
+(define (factorial number)
+    (factorial-iterator 1 1 number))
+
+(factorial 5)
+
+(define consData (cons 2 5))
+(car consData)
+(cdr consData)
+
+(cons 1 (list 2 3))
+
+(define listData (list 1 2 3))
+listData
+; 1
+(car listData)
+; (2 3)
+(cdr listData)
+; (1 2 3)
+(cons (car listData)
+            (cdr listData))
+(car (cdr listData))
+; 3
+(car (cdr (cdr listData)))
+; ()
+(cdr (cdr (cdr listData)))
+(eq? (cdr (cdr (cdr listData))) '())
+(null? (cdr (cdr (cdr listData))))
+
+; (map (lambda (x) (+ x 1)) (1 2 3))
+
+(define (square x) 
+    (* x x))
+
+; (map procedure mylist)
+(define (map procedure mylist)
+    (if (null? mylist)
+        '()
+        (cons (procedure (car mylist))
+            (map procedure (cdr mylist)))))
+(map square (list 1 2 3))
+
+; filter
