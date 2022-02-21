e91fe78767418fbdba57334347cabe1f2f103461
codethereforam <codethereforam@gmail.com>
2022-02-21 17:58:14 +0800

    feat: scheme判断是否是素数

--- scheme/$.scm
+++ scheme/$.scm
@@ -1,2 +1,9 @@
 (define (square x)
     (* x x))
+
+(define (prime? x)
+    (define (loop i)
+        (cond ((> (square i) x) #t)
+            ((= (modulo x i) 0) #f)
+            (else (loop (+ i 1)))))
+    (loop 2))
