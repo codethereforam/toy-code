6cd9748e98e89443985245ef1b12f5212c0fa460
codethereforam <codethereforam@gmail.com>
2023-08-24 23:25:35 +0800

    refactor: 格式化代码

--- python/word/lemmatize_sentence.py
+++ python/word/lemmatize_sentence.py
@@ -8,6 +8,7 @@ from typing import Set
 
 lemmatizer = WordNetLemmatizer()
 
+
 # function to convert nltk tag to wordnet tag
 def nltk_tag_to_wordnet_tag(nltk_tag):
     if nltk_tag.startswith('J'):
@@ -21,25 +22,28 @@ def nltk_tag_to_wordnet_tag(nltk_tag):
     else:
         return None
 
+
 def lemmatize_sentence_list(sentence):
-    #tokenize the sentence and find the POS tag for each token
+    # tokenize the sentence and find the POS tag for each token
     nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))
-    #tuple of (token, wordnet_tag)
+    # tuple of (token, wordnet_tag)
     wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
     lemmatized_sentence = []
     for word, tag in wordnet_tagged:
         if tag is None:
-            #if there is no available tag, append the token as is
+            # if there is no available tag, append the token as is
             converted_word = word
         else:
-            #else use the tag to lemmatize the token
+            # else use the tag to lemmatize the token
             converted_word = lemmatizer.lemmatize(word, tag)
         # todo!: 在这里移除 's 等
         lemmatized_sentence.append(converted_word.lower())
     return lemmatized_sentence
 
+
 def lemmatize_sentence(sentence):
     return " ".join(lemmatize_sentence_list(sentence))
+
 
 # print(lemmatizer.lemmatize("I am loving it")) #I am loving it
 # print(lemmatizer.lemmatize("loving")) #loving
@@ -49,6 +53,7 @@ def lemmatize_sentence(sentence):
 
 def lemmatize_sentence_set(sentence):
     return list(dict.fromkeys(lemmatize_sentence_list(sentence)))
+
 
 def get_unique_words_set(file_path: str) -> Set[str]:
     with open(file_path, "r", encoding="utf-8") as file:
@@ -71,11 +76,14 @@ def extract_word_from_article(file):
 
 unique_words_set = get_unique_words_set(word_file)
 word_list = extract_word_from_article(article_file)
+
+
 # print("\n".join(unique_words_set))
 # print("\n".join(word_list)
 
 def find_elements_not_in_set(input_list, input_set):
     result = [word for word in input_list if word.strip() and word.strip().lower() not in input_set]
     return result
+
 
 print("\n".join(find_elements_not_in_set(word_list, unique_words_set)))
